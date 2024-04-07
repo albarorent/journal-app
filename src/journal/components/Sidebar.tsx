03aede8160ef69ab0312867429446caf693db0c2
Alvaro <alvarorent2001@gmail.com>
2024-04-07 17:00:51 -0500

    update

--- src/journal/components/Sidebar.tsx
+++ src/journal/components/Sidebar.tsx
@@ -28,6 +28,7 @@ export const Sidebar = ({
     <Box
       component="nav"
       sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      className="animate__animated animate__backOutLeft"
     >
       <Drawer
         variant="permanent"
